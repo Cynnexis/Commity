82f2d765f7363a08ebaced5f52f7a041df5818fc
Valentin Berger <Valentin.berger38@gmail.com>
2020-05-21 10:10:02 +0200

    :bug: Fix `IssueTrackerTest`
    * :white_check_mark: Updated the tests.

--- test/issue_tracker_test.py
+++ test/issue_tracker_test.py
@@ -10,15 +10,17 @@ class IssueTrackerTest(unittest.TestCase):
 		super(IssueTrackerTest, self).__init__(*args, **kwargs)
 	
 	def test_get_issues(self):
-		self.assertEqual(get_issues("Fixed #36"), [36])
-		self.assertEqual(get_issues("Fixed #1."), [1])
+		self.assertEqual(get_issues("Fixed #36"), ["#36"])
+		self.assertEqual(get_issues("Fixed #1."), ["#1"])
 		self.assertEqual(get_issues("Fixed #-1."), [])
 		self.assertEqual(get_issues("Fixed #."), [])
 		self.assertEqual(get_issues("Fixed ####."), [])
-		self.assertEqual(get_issues("Fixed ##35##."), [35])
+		self.assertEqual(get_issues("Fixed ##35##."), ["#35"])
 		self.assertEqual(get_issues("Fixed # 35."), [])
-		self.assertEqual(get_issues("Fixed #45, #25 and #92."), [45, 25, 92])
-		self.assertEqual(get_issues("Fixed #1, but #1 will stay open just in case the bug re-appears."), [1])
+		self.assertEqual(get_issues("Fixed #45, #25 and #92."), ["#45", "#25", "#92"])
+		self.assertEqual(
+			get_issues("Fixed #45, user/project#25 and other/idea#92."), ["#45", "user/project#25", "other/idea#92"])
+		self.assertEqual(get_issues("Fixed #1, but #1 will stay open just in case the bug re-appears."), ["#1"])
 
 
 if __name__ == '__main__':
